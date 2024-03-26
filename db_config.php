b7ebbf520d253da96db2cc8ed9979166bed580cb
Ghun Assudani <141937659+Ghun-Assudani7@users.noreply.github.com>
2024-03-27 00:19:43 +0530

    db_config.php

--- db_config.php
+++ db_config.php
@@ -19,6 +19,14 @@ function filteration($data)
         }
         return $data;
 }
+
+function selectAll($table)
+{
+    $con=$GLOBALS['con'];
+    $res=mysqli_query($con,"SELECT * FROM $table");
+    return $res;
+}
+
 function select($sql, $values, $datatypes)
 { 
 
@@ -56,21 +64,42 @@ function update($sql, $values, $datatypes)
     }
 }
 
-// function insert($sql, $values, $datatypes)
-// { 
-//     $con = $GLOBALS['con'];
-//     if ($stmt = mysqli_prepare($con, $sql))
-//     {mysqli_stmt_bind_param($stmt, $datatypes,... $values);
-//         if(mysqli_stmt_execute($stmt)){
-//             $res = mysqli_stmt_affected_rows($stmt);
-//             mysqli_stmt_close($stmt);
-//             return $res;
-//         }else{
-//         mysqli_stmt_close($stmt);
-//         die("Query cannot be executed - Insert");
-//         }
-//     }else{
-//         die("Query cannot be prepared - Insert");
-//     }
-// }
+function insert($sql, $values, $datatypes)
+{ 
+    $con = $GLOBALS['con'];
+    if ($stmt = mysqli_prepare($con, $sql))
+    {mysqli_stmt_bind_param($stmt, $datatypes,... $values);
+        if(mysqli_stmt_execute($stmt)){
+            $res = mysqli_stmt_affected_rows($stmt);
+            mysqli_stmt_close($stmt);
+            return $res;
+        }else{
+        mysqli_stmt_close($stmt);
+        die("Query cannot be executed - Insert");
+        }
+    }else{
+        die("Query cannot be prepared - Insert");
+    }
+}
+
+function delete($sql, $values, $datatypes)
+{ 
+    $con = $GLOBALS['con'];
+    if ($stmt = mysqli_prepare($con, $sql))
+    {
+        
+        mysqli_stmt_bind_param($stmt, $datatypes,... $values);
+        if(mysqli_stmt_execute($stmt)){
+            $res = mysqli_stmt_affected_rows($stmt);
+            mysqli_stmt_close($stmt);
+            return $res;
+        }else{
+            mysqli_stmt_close($stmt);
+            die("Query cannot be executed - delete");
+        }
+    }else{
+        die("Query cannot be prepared - delete");
+    }
+}
+
 ?>
