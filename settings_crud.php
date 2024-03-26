8b57bee0c36e2e6e1991ff2bffe96100c81cabcd
Ghun Assudani <141937659+Ghun-Assudani7@users.noreply.github.com>
2024-03-27 00:18:13 +0530

    ]settings_crud.php

--- settings_crud.php
+++ settings_crud.php
@@ -92,23 +92,103 @@ if (isset($_POST['add_member']))
     $img_r=uplpoadImage($_FILES['picture'],ABOUT_FOLDER);
     // $img_r=uplpoadImage($_FILES['picture'],);
 
+    // $img_r=uplpoadImage($_FILES['picture'],);
 
-    // if($img_r=='inv_img'){
-    //     echo $img_r;
-    // }
-    // else if($img_r =='inv_size'){
-    //     echo $img_r;
-    // }
-    // else if($img_r =='upd_failed'){
-    //     echo $img_r;
-    // }
-    // else{
-    //     $q="INSERT INTO `team_details`(`name`, `picture`) VALUES (?,?)";
-    //     $values=[$frm_data['name'],$img_r];
-    //     $res=insert($q,$values,'ss');
-    //     echo $res;
-    // }
+
+    if($img_r=='inv_img'){
+        echo $img_r;
+    }
+    else if($img_r =='inv_size'){
+        echo $img_r;
+    }
+    else if($img_r =='upd_failed'){
+        echo $img_r;
+    }
+    else{
+        $q="INSERT INTO `team_details`(`name`, `picture`) VALUES (?,?)";
+        $values=[$frm_data['name'],$img_r];
+        $res=insert($q,$values,'ss');
+        echo $res;
+    }
 
     
 }
+
+// if (isset($_POST['get_members'])) 
+// {
+//     $res=selectAll('team_details');
+
+//     while($row=mysqli_fetch_assoc($res))
+//     {
+//         $path=ABOUT_IMG_PATH;
+
+//         echo <<<data
+//             <div class="col-md-2 mb-3">
+//                 <div class="card bg-dark text-white">
+//                     <img src="$path$row[picture];" class="card-img" alt="Team member">    
+//                     div class="card-img-overlay text-end">
+//                         <button type="button" class="btn btn-danger btn-sm shadow-sm shadow-none">
+//                             <i class="bi bi-trash"></i>Delete
+//                         </button>
+//                     </div>
+//                     <p class="card-text text-center px-1 py-1">$row[name] </p>
+//                 </div>
+//             </div>
+//         data;
+//     }
+// }
+
+if (isset($_POST['get_members'])) 
+{
+    $res = selectAll('team_details');
+
+    while ($row = mysqli_fetch_assoc($res)) 
+    {
+        $path=ABOUT_IMG_PATH;
+        $path2=ABOUT_IM_PATH;
+        echo <<<data
+            <div class="col-md-2 mb-3">
+                <div class="card bg-dark text-white">
+                <img src="$path2$row[picture]" class="card-img">
+
+                    <div class="card-img-overlay text-end">
+                        <button type="button" onclick="rem_member($row[sr_no])" class="btn btn-danger btn-sm shadow-sm shadow-none">
+                            <i class="bi bi-trash"></i>Delete
+                        </button>
+                    </div>
+                    <p class="card-text text-center px-1 py-1">$row[name] </p>
+                </div>
+            </div>
+        data;
+    }
+}
+
+if (isset($_POST['rem_member'])) {
+    $frm_data = filteration($_POST);
+    $values =[$frm_data['rem_member']];
+    // $values = [$frm_data];
+
+    $pre_q = "SELECT * FROM `team_details` WHERE `sr_no`=?";
+    $res = select($pre_q, $values, 'i');
+    $img = mysqli_fetch_assoc($res);
+    print_r($img); // Output the fetched data for debugging
+
+    if (deleteImage($img['picture'], ABOUT_FOLDER)) {
+    // if (deleteImage($img['picture'], $folder = "frontend photos/about/")) {
+        $q = "DELETE FROM `team_details` WHERE `sr_no`=?";
+        echo $q;
+        // $res = delete($pre_q, $values, 'i');
+        $res = delete($q, $values, 'i');
+
+        echo "hhh$res";
+    } else {
+        echo 0;
+    }
+}
+
+
+
+
+
+
 ?>
