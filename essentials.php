b44e6c8aecc94ef3cc877d8dfb23f77223734524
Ghun Assudani <141937659+Ghun-Assudani7@users.noreply.github.com>
2024-03-27 00:18:59 +0530

    essentials.php

--- essentials.php
+++ essentials.php
@@ -1,8 +1,21 @@
 
 <?php
 
-// define('UPLOAD_IMAGE_PATH',$_SERVER('DOCUMENT_ROOT'));
-define('ABOUT_FOLDER','about/');
+//frontend purpose data
+define('SITE_URL','https://127.0.0.1/harbor-lights-master/');
+define('ABOUT_IMG_PATH',SITE_URL.'frontend photos/about/');
+define('ABOUT_IM_PATH','/frontend photos/about/');
+
+
+// define('ABOUT_IMG_PATH',SITE_URL.'/frontend photos/about/team.jpg');
+
+
+
+//backend upload process needs data
+
+define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/frontend photos');
+define('ABOUT_FOLDER', '/about/');
+
 
     function adminLogin(){
         session_start();
@@ -48,7 +61,7 @@ define('ABOUT_FOLDER','about/');
             return 'inv_size';//invalid size greater than 2mb
         }
         else{
-            $ext=pathinfo($image['name'].PATHINFO_EXTENSION);
+            $ext=pathinfo($image['name'],PATHINFO_EXTENSION);
             $rname='IMG_'.random_int(11111,99999).".$ext";
 
             $img_path= UPLOAD_IMAGE_PATH.$folder.$rname;
@@ -60,6 +73,36 @@ define('ABOUT_FOLDER','about/');
             }
         }
     }
+
+    function deleteImage($image,$folder){
+        echo UPLOAD_IMAGE_PATH.$folder.$image;
+        if(unlink(UPLOAD_IMAGE_PATH.$folder.$image)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    // $image=$img['picture'];
+    // // $image = '\IMG_74333.jpg'; // Replace "team.jpg" with the actual filename
+    // $folder = "about";  
+    // $upd = 'C:\Users\HP\harbor-lights-master\frontend photos'; // Updated path, using forward slashes for directory separators
+
+    // $result = deleteImage($image, $folder); // Call deleteImage() function and store the return value in $result
+    // echo "Return value: " . ($result ? "true" : "false"); 
+
+    // function deleteImage($image, $folder) {
+    // global $upd; // Access the $upd variable inside the function
+    // $file_path = $upd . '/' . $folder . $image; // Construct the full file path
+    
+    // echo "Deleting file: " . $file_path; // Print the path of the file being deleted
+    // 'C:\Users\HP\harbor-lights-master\frontend photos\about\IMG_74333.jpg';
+    // if(unlink($file_path)) { // Attempt to delete the file
+    //     return true; // Return true if deletion is successful
+    // } else {
+    //     return false; // Return false if deletion fails
+    // }
+// }
 
     
 ?>
